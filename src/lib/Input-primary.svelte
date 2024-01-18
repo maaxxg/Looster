8cdfa9b5f516bdfaace992105d06dad44bbba4bd
maaxxg <makgregoric@gmail.com>
2024-01-18 20:53:12 +0100

    input main done

---
+++ src/lib/Input-primary.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let placeholder = '';
+    export let type = "text"
+</script>
+<input type="{type}" placeholder="{placeholder}">
+
+<style>
+    input {
+        border: none;
+        outline: none;
+        background-color: #6F6D6D;
+        color: var(--foreground-color);
+        border: var(--primary-color) 1px solid;
+        border-radius: 5px;
+        padding: 8px 15px;
+    }
+    input::placeholder {
+        color: #aaa;
+    }
+</style>
