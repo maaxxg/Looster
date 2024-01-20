5cdfcd1726a592f5c2dc3d91316c33f0353b91f1
maaxxg <makgregoric@gmail.com>
2024-01-20 16:39:29 +0100

    Long logo done

---
+++ src/lib/Logo-long.svelte
@@ -0,0 +1,30 @@
+<div class="logo-long">
+    <h1 class="logo-heading">CHATAPP</h1>
+    <p class="logo-p">The new way of <span class="logo-span">instant<br> messaging</span></p>
+</div>
+
+<style>
+    h1{
+        font-size: 28px;
+        color: var(--primary-color);
+        margin: 0;
+        padding: 0;
+    }
+
+    .logo-long {
+        display: flex;
+        align-items: center;
+    }
+
+    p {
+        margin-left: 12px;
+        line-height: 22px;
+        font-weight: 200;
+        letter-spacing: .5px;
+    }
+
+    span {
+        color: var(--primary-color);
+        font-weight: 400;
+    }
+</style>
