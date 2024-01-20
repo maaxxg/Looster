cc60dd1278a549ea82cfcd53c19a5f53dfc9eac1
maaxxg <makgregoric@gmail.com>
2024-01-20 16:17:51 +0100

    links done

---
+++ src/lib/Link.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let href = "/"
+</script>
+<a on:click href="{href}">
+    <slot></slot>
+</a>
+
+<style>
+    a {
+        text-decoration: none;
+        color: var(--foreground-color);
+        font-weight: 200;
+        letter-spacing: .7px;
+        font-size: 18px;
+        transition: .3s;
+        margin: 0 10px;
+    }
+
+    a:hover {
+        opacity: 0.8;
+    }
+</style>
