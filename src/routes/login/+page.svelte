20f92b10b7eba655a99875eebc058c8335706b89
cenkovili <cenkovili11@gmail.com>
2024-01-31 23:19:38 +0100

    Sajnap i login bato

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -8,5 +8,39 @@
     import Modal from "$lib/Modal.svelte"
     import ButtonPrimary from "$lib/Primary-button.svelte"
     import ButtonSecondary from "$lib/Secondary-button.svelte"
+    
 </script>
-<h1>Login</h1>
+<LogoLong />
+<div class="container">
+
+    <Card>
+        <label>Username</label>
+        <InputPrimary />
+        <label>Password</label>
+        <InputPrimary />
+        <ButtonPrimary message="Login"/>
+        <a href="https://pornhub.com" id="lol">Trouble logging in?</a>
+    </Card>
+    <p>Dont have an account? <a href="/signup"><span class="accent">Create one!</span></a></p>
+</div>
+<style>
+    .container{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 95vh;
+    }
+    a{
+        text-decoration: none;
+        color: white;
+    }
+    .accent {
+        color: var(--primary-color);
+        font-weight: 350;}
+    #lol{
+        font-size: 12px;
+        color: var(--primary-color);
+    }
+    
+</style>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -9,4 +9,40 @@
     import ButtonPrimary from "$lib/Primary-button.svelte"
     import ButtonSecondary from "$lib/Secondary-button.svelte"
 </script>
-<h1>Signup</h1>
+<LogoLong />
+<div class="container">
+
+    <Card>
+        <label>Username</label>
+        <InputPrimary />
+        <label>Email adress</label>
+        <InputPrimary />
+        <label>Password</label>
+        <InputPrimary />
+        <ButtonPrimary message="Signup"/>
+        <p>Accept <a href="https://www.primasolvent.hr/en/home/" id="lol">Terms and Conditions</a><input type="checkbox"/>
+    </p>
+        </Card>
+    <p>Already have an account? <a href="/login"><span class="accent">Login!</span></a></p>
+</div>
+<style>
+    .container{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 95vh;
+    }
+    a{
+        text-decoration: none;
+        color: white;
+    }
+    .accent {
+        color: var(--primary-color);
+        font-weight: 350;}
+    #lol{
+        font-size: 16px;
+        color: var(--primary-color);
+    }
+    
+</style>
