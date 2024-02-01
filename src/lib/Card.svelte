4ddadc54b782c22f480975f6b42a2e451c211615
maaxxg <makgregoric@gmail.com>
2024-02-01 17:07:52 +0100

    signup i login gotov

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -7,7 +7,7 @@
     .card {
         width: fit-content;
         border-radius: 10px;
-        padding: 30px 30px;
+        padding: 50px 30px;
         box-shadow: -1px 2px 5px rgba(0, 0, 0, 0.7);
         display: flex;
         flex-direction: column;
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -10,37 +10,61 @@
     import ButtonSecondary from "$lib/Secondary-button.svelte"
     
 </script>
-<LogoLong />
+<div class="logoContainer">
+    <a href="/">
+        <LogoLong />
+    </a>
+</div>
 <div class="container">
 
     <Card>
-        <label>Username</label>
-        <InputPrimary />
-        <label>Password</label>
-        <InputPrimary />
-        <ButtonPrimary message="Login"/>
-        <a href="https://pornhub.com" id="lol">Trouble logging in?</a>
+        <div class="innerCard">
+            <span>Username</span>
+            <InputPrimary placeholder="John Doe" />
+            <span>Password</span>
+            <InputPrimary placeholder="............" />
+            <br>
+            <ButtonPrimary message="Login"/>
+            <span>
+                <a href="/" id="troubleLogin">Trouble logging in?</a>
+            </span>
+        </div>
     </Card>
     <p>Dont have an account? <a href="/signup"><span class="accent">Create one!</span></a></p>
 </div>
 <style>
+    .logoContainer {
+        height: 10vh;
+    }
+
+    .innerCard {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        width: 260px;
+        height: 240px;
+    }
     .container{
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        height: 95vh;
+        height: 90vh;
     }
     a{
         text-decoration: none;
         color: white;
+        transition: all .5s;
     }
     .accent {
         color: var(--primary-color);
         font-weight: 350;}
-    #lol{
-        font-size: 12px;
+    #troubleLogin{
+        font-size: 16px;
         color: var(--primary-color);
     }
     
+    a:hover {
+        opacity: 0.7;
+    }
 </style>
